106e2a7204fd6939766a3d14ed2ab86313fd2ef1
Adnan <adnan.hussain.136660@gmail.com>
2024-05-02 16:14:16 +0530

    fixed bug in server search component

--- components/server/ServerSearch.tsx
+++ components/server/ServerSearch.tsx
@@ -57,11 +57,11 @@ const ServerSearch = ({ data }: ServerSearchProps) => {
     setOpen(false);
 
     if (type === "member") {
-      return router.push(`/server/${params?.serverId}/conversations/${id}`);
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`);
     }
 
     if (type === "channel") {
-      return router.push(`/server/${params?.serverId}/channels/${id}`);
+      return router.push(`/servers/${params?.serverId}/channels/${id}`);
     }
   };
 
